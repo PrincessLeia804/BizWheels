03f47bd610bc1da13a3f7d3db08cc8628d017034
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-07-13 14:21:46 +0100

    Setting dates

--- routes/cars.route.js
+++ routes/cars.route.js
@@ -6,9 +6,16 @@ router.get("/", (req, res) => {
   res.json("Cars index will be here");
 });
 
-router.get("/request", (req, res) => {
-  res.render("cars/car_request", { availableCars: null });
-});
+router.get("/request", async (req, res) => {
+  const today = new Date(); 
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate()+1);
+  const availableCars = await checkCarAvailability(today, tomorrow);
+  
+  const startDate = today.toISOString().split('T')[0];
+  const endDate = tomorrow.toISOString().split('T')[0];
+  res.render('cars/car_request', { startDate, endDate, availableCars  });
+  });
 
 router.post("/request", async (req, res) => {
   const { startDate, endDate } = req.body;
@@ -16,7 +23,7 @@ router.post("/request", async (req, res) => {
   try {
     const availableCars = await checkCarAvailability(startDate, endDate);
 
-    res.render("cars/car_request", { availableCars: availableCars });
+    res.render("cars/car_request", {startDate, endDate, availableCars });
   } catch (error) {
     console.log("Error processing car request:", error);
     res.status(500).send("Error processing car request");
