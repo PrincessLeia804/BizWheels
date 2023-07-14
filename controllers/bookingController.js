6872620d970c7d998606111505382126dafb3154
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-07-14 12:37:49 +0100

    Added some more validations for bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -23,20 +23,20 @@ async function createBooking(carId, employeeId, startDate, endDate) {
 async function validateBooking(carId, employeeId, startDate, endDate) {
   try {
     // Validate car
-    const car = Car.findById(carId);
+    const car = await Car.findById(carId);
     
     if (!car){
       throw new Error("Car doesn't exist");
     }
 
     // Validate employee
-    const employee = User.findById(employeeId);
+    const employee = await User.findById(employeeId);
     
     if (!employee){
       throw new Error("Employee doesn't exist");
     }
 
-    // Validate if start date not in the past, and that end date bigger than start date
+    // Validate if start date not in the past, and that end date is bigger than start date
     const today = new Date().setHours(0, 0, 0, 0);
     const selectedStartDate = new Date(startDate).setHours(0, 0, 0, 0);
     const selectedEndDate = new Date(endDate).setHours(0, 0, 0, 0);
@@ -49,16 +49,20 @@ async function validateBooking(carId, employeeId, startDate, endDate) {
       throw new Error('The selected end date must be after the start date.');
     }
 
-    const existingBooking = await Booking.findOne({
+    const existingCarBooking = await Booking.findOne({
       carId,
       startDate: { $lte: endDate },
       endDate: { $gte: startDate }
     });
 
-    if (existingBooking) {
+    if (existingCarBooking) {
       throw new Error('This car is already booked for the selected dates.');
     }
 
+    const existingUserBookings = await checkUserExistingBookings(employeeId, startDate, endDate);
+    if (existingUserBookings.length > 0){
+      throw new Error('Employee already has a booking for the selected dates.');
+    }
 
     return true;
   } catch (error) {
@@ -67,7 +71,40 @@ async function validateBooking(carId, employeeId, startDate, endDate) {
   }
 }
 
+async function checkExistingBookings(startDate, endDate) {
+  try {
+
+    const existingBookings = await Booking.find({
+      $or: [
+        { startDate: { $lte: endDate }, endDate: { $gte: startDate } }, // Booking overlaps with specified dates
+        { startDate: { $gte: startDate, $lte: endDate } }, // Booking starts within specified dates
+        { endDate: { $gte: startDate, $lte: endDate } } // Booking ends within specified dates
+      ]
+    });
+    return existingBookings;
+
+  } catch (error) {
+    console.log('Fail to check existing bookings:', error);
+    throw error;
+  }
+}
+
+async function checkUserExistingBookings(userId, startDate, endDate) {
+  try {
+    const existingBookings = await checkExistingBookings(startDate, endDate);
+    
+    const userBookings = existingBookings.filter(booking => booking.employeeId.equals(userId));
+    
+    return userBookings;
+  } catch (error) {
+    console.log('Failed to check user existing bookings:', error);
+    throw error;
+  }
+}
+
 
 module.exports = {
-  createBooking,
+  createBooking, 
+  checkExistingBookings, 
+  checkUserExistingBookings
 };
