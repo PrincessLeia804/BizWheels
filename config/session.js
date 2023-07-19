f119c95063e4dc22167d0e5749c2bfed69e2a2ca
Lea Model <lea.skywalker309@gmail.com>
2023-07-19 15:21:24 +0200

    Testing: Fixing DB-issues

--- config/session.js
+++ config/session.js
@@ -19,7 +19,7 @@ module.exports = app => {
         //maxAge: 100000 // 60 * 1000 ms === 1 min
       },
       store: MongoStore.create({
-        mongoUrl: "mongodb+srv://bizwheels-main-db-01e5d5349eb:6XR2S8Fe2K9cpqq3QX5V7S28HQkgEW@prod-us-central1-2.ih9la.mongodb.net/bizwheels-main-db-01e5d5349eb",
+        mongoUrl: process.env.MongoDB_URI || "mongodb://127.0.0.1:27017/bizwheels",
         ttl: 60 * 60 * 24 * 1 // 60sec * 60min * 24day => 1 day
       })
     })
